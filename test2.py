54079adc6e75a95cfd91f147105a3b060e3a4a4a
mschuchin <Foreva11>
2021-11-16 15:33:35 +0300

    test2.py refactoring 2

--- test2.py
+++ test2.py
@@ -1,12 +1,8 @@
-'''fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => console.log(json));'''
-import json
-
 import requests
 
 response = requests.request(method="GET", url='https://jsonplaceholder.typicode.com/posts')
-body = json.loads(response.text)
+body = response.json()
 result = [i for i in body if i['userId'] == 1 and i['id'] == 5]
-del result[0]['userId'], result[0]['id']
+for _ in result:
+    del _['userId'], _['id']
 print(result)
